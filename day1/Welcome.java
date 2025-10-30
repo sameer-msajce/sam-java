5e29c5536294fba26c1d1d1181c69e1ec6451683
sameer-msajce <311823205007@msajce-edu.in>
2025-10-30 15:03:33 +0530

    Create Welcome.java

---
+++ day1/Welcome.java
@@ -0,0 +1,9 @@
+public class Welcome {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Welcome " + args[0]);
+        } else {
+            System.out.println("Please provide a name as a command-line argument.");
+        }
+    }
+}
