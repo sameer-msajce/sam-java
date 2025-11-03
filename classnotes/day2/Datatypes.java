1ce75822c1a028ab9e728f76dbbe3553a5ed4e8f
sameer-msajce <311823205007@msajce-edu.in>
2025-11-03 14:39:48 +0530

    Create Datatypes.java

---
+++ classnotes/day2/Datatypes.java
@@ -0,0 +1,20 @@
+ public class Datatypes {
+  public static void main(String args[]){
+    System.out.println("This is printed by print statement");
+    System.out.println("Datatypes");
+    int num=20;
+    System.out.printf("This is Integer %d\n",num);
+    short numshort=365;
+    System.out.printf("This is Short Integer %d\n",numshort);
+    long numlong=3342;
+    System.out.printf("This is Long Integer %d\n",numlong);
+    float numdecimal=3.56f;
+    System.out.printf("this is Float %f\n",numdecimal);
+    double numdouble=44.32;
+    System.out.printf("This is Double %f\n",numdouble);
+    char charac='A';
+    System.out.printf("This is character %c\n",charac);
+   boolean sam= true;
+   System.out.printf("This is Boolean %b\n",sam);
+   
+    }}
