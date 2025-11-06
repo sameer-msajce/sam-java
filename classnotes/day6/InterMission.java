c399838ac1f0d96267ae5371a6884ec6ce42487e
sameer-msajce <311823205007@msajce-edu.in>
2025-11-06 12:47:36 +0530

    Update InterMission.java

--- classnotes/day6/InterMission.java
+++ classnotes/day6/InterMission.java
@@ -1,27 +1,15 @@
 import java.util.*;
+import java.util.stream.*;
 
 public class InterMission {
 
     public static int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> set1 = new HashSet<>();
-        for (int num : nums1) {
-            set1.add(num);
-        }
+        Set<Integer> set1 = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
 
-        Set<Integer> result = new HashSet<>();
-        for (int num : nums2) {
-            if (set1.contains(num)) {
-                result.add(num);
-            }
-        }
-
-        int[] output = new int[result.size()];
-        int i = 0;
-        for (int num : result) {
-            output[i++] = num;
-        }
-
-        return output;
+        return Arrays.stream(nums2)
+                     .distinct()
+                     .filter(set1::contains)
+                     .toArray();
     }
 
     public static void main(String[] args) {
@@ -29,6 +17,6 @@ public class InterMission {
         int[] nums2 = {9, 4, 9, 8, 4};
 
         int[] ans = intersection(nums1, nums2);
-        System.out.println(Arrays.toString(ans)); 
+        System.out.println(Arrays.toString(ans));
     }
 }
