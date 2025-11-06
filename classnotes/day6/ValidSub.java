ee9831e1a495a7938085ece8561cf82bad95b388
sameer-msajce <311823205007@msajce-edu.in>
2025-11-06 12:41:40 +0530

    Update and rename ValidAnagram.java to ValidSub.java

--- classnotes/day6/ValidSub.java
+++ classnotes/day6/ValidSub.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class ValidAnagram {
+public class ValidSub {
 
     public static boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
